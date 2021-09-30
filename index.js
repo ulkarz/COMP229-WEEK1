700eee996ed7437a7d46968d0194c383df0e5124
UlkarZakaryayeva <uzakarya@my.centennialcollege.ca>
2021-09-29 23:44:51 -0400

    my first commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const http = require('http');
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
 const hostname = '127.0.0.1';
 const port = 3000;
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+console.log(path_1.default.dirname);
+const server = http_1.default.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
 });
-
-let myVariable = server.httplock;
-console.log(myVariable);
-
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,18 @@
+import http from 'http';
+import path from 'path';
+
+const hostname: string = '127.0.0.1';
+const port: number = 3000;
+
+console.log(path.dirname);
+
+const server: http.Server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
